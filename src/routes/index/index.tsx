934a320818a0d666481274bf697c1c9587f3bb87
Hadrien Boutteville <h.boutteville@leosidea.com>
2024-03-25 12:34:43 +0100

    refactor: clean index

--- src/routes/index/index.tsx
+++ src/routes/index/index.tsx
@@ -1,10 +1,14 @@
+import { ActionFunctionArgs } from "react-router-dom";
+
+export async function action({ request }: ActionFunctionArgs) {}
+
 export default function RootIndex() {
   return (
-      <section>
-        <h2 className="text-lg">Title</h2>
-        <p className="text-sm">
-          Hello b.
-        </p>
-      </section>
+    <section>
+      <h1 className="flex flex-col items-center content-center text-3xl my-5 font-bold">
+        HRnet
+      </h1>
+      <p className="text-sm">Hello b.</p>
+    </section>
   );
 }
